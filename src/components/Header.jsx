22ac192c4ee3d8fb099f680d003275a0ffab282a
GonzalokHM <gonzalohm93@gmail.com>
2024-02-03 22:04:38 +0100

    first test

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+const Header = () => {
+  return (
+    <header>
+        <h1>LandingBooster</h1>
+        <img src="https://cdn-icons-png.flaticon.com/512/11744/11744612.png" alt="booster icon" />
+    </header>
+  )
+}
+
+export default Header
